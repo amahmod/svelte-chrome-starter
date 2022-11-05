3852cfdbda40547219fb412c9ccf0e01b96e55b4
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-11-05 23:23:10 +0600

    chore: 4 space indentation

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,21 +2,21 @@
  * @type {import('prettier').Options}
  */
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'none',
-  bracketSpacing: true,
-  bracketSameLine: true,
-  arrowParens: 'avoid',
-  plugins: [
-    require.resolve('@plasmohq/prettier-plugin-sort-imports'),
-    require.resolve('prettier-plugin-svelte')
-  ],
-  importOrder: ['^@plasmohq/(.*)$', '^~(.*)$', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  svelteSortOrder: 'options-scripts-markup-styles'
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: 'avoid',
+    plugins: [
+        require.resolve('@plasmohq/prettier-plugin-sort-imports'),
+        require.resolve('prettier-plugin-svelte')
+    ],
+    importOrder: ['^@plasmohq/(.*)$', '^~(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    svelteSortOrder: 'options-scripts-markup-styles'
 }
