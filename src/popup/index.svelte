a82d972bb2542fb54a29af637ec3b8162811638f
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2022-11-05 23:36:30 +0600

    feat: tailwind css

--- src/popup/index.svelte
+++ src/popup/index.svelte
@@ -1,1 +1,13 @@
-<h1>Popup Page</h1>
+<script lang="ts">
+    import '../tailwind.css'
+</script>
+
+<h1 class="text-teal-500 text-2xl text-center">Popup Page</h1>
+
+<style>
+    :global(body) {
+        background-color: #f5f5f5;
+        min-width: 300px;
+        min-height: 400px;
+    }
+</style>
